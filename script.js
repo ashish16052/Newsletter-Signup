b23ba2d34e5a70e8354aaf601ae15569750ce685
Ashish Mohite <86217607+ashish16052@users.noreply.github.com>
2021-11-22 13:25:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const https = require("https");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+mailchimp.setConfig({
+    apiKey: "078d3431a42cb55f981e3bd1bab7d1371-us20",
+    server: "us20"
+});
+
+app.post("/", function (req, res) {
+
+    const firstName = req.body.first;
+    const secondName = req.body.last;
+    const email = req.body.email;
+    const listId = "fb31a23084";
+    const subscribingUser = {
+        firstName: firstName,
+        lastName: secondName,
+        email: email
+    };
+
+    async function run() {
+        const response = await mailchimp.lists.addListMember(listId, {
+            email_address: subscribingUser.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: subscribingUser.firstName,
+                LNAME: subscribingUser.lastName
+            }
+        })
+    };
+    run();
+
+    res.sendFile(__dirname + "/success.html");
+    run().catch(e => res.sendFile(__dirname + "/failure.html"));
+})
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port-3000");
+});
+
+//78d3431a42cb55f981e3bd1bab7d1371-us20
+//fb31a23084  
